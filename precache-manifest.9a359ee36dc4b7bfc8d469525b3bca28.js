c689a2d8fba660af6123f211f7d1225dbbb279fa
Jimmy Zebroski <zebrozkii@gmail.com>
2019-08-14 18:49:32 -0700

    Updates

---
+++ precache-manifest.9a359ee36dc4b7bfc8d469525b3bca28.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80abbbdb696dba6a4a037837e3b30123",
+    "url": "/Monster-Rolodex/index.html"
+  },
+  {
+    "revision": "9583373949c55ea5a2ab",
+    "url": "/Monster-Rolodex/static/css/main.a8075fa1.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/Monster-Rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "9583373949c55ea5a2ab",
+    "url": "/Monster-Rolodex/static/js/main.d3a8f1f5.chunk.js"
+  },
+  {
+    "revision": "1c53a097523ea440329e",
+    "url": "/Monster-Rolodex/static/js/runtime~main.5e73536d.js"
+  }
+]);
